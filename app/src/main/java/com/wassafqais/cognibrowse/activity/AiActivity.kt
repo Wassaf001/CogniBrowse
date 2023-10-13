37a87ac7786226e6380c3605b8e9b9f7e987dbbf
wassaf <wassafali213@gmail.com>
2023-10-13 22:27:20 +0530

    Fifth

--- app/src/main/java/com/wassafqais/cognibrowse/activity/AiActivity.kt
+++ app/src/main/java/com/wassafqais/cognibrowse/activity/AiActivity.kt
@@ -8,64 +8,15 @@ import android.view.inputmethod.EditorInfo
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textview.MaterialTextView
 import com.wassafqais.cognibrowse.R
-import com.wassafqais.cognibrowse.model.Bookmark
-import com.wassafqais.cognibrowse.model.Tab
 import okhttp3.*
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.IOException
-import java.util.ArrayList
-import android.annotation.SuppressLint
-import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.os.Build
-import android.print.PrintAttributes
-import android.print.PrintJob
-import android.print.PrintManager
-import android.view.Gravity
-import android.view.WindowManager
-import android.webkit.WebView
-import androidx.appcompat.app.AlertDialog
-import androidx.core.content.ContextCompat
-import androidx.core.content.res.ResourcesCompat
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.Lifecycle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.snackbar.Snackbar
-import com.google.gson.GsonBuilder
-import com.google.gson.reflect.TypeToken
-import com.wassafqais.cognibrowse.activity.MainActivity.Companion.myPager
-import com.wassafqais.cognibrowse.activity.MainActivity.Companion.tabsBtn
-import com.wassafqais.cognibrowse.adapter.TabAdapter
-import com.wassafqais.cognibrowse.databinding.ActivityMainBinding
-import com.wassafqais.cognibrowse.databinding.BookmarkDialogBinding
-import com.wassafqais.cognibrowse.databinding.MoreFeaturesBinding
-import com.wassafqais.cognibrowse.databinding.TabsViewBinding
-import com.wassafqais.cognibrowse.fragment.BrowseFragment
-import com.wassafqais.cognibrowse.fragment.HomeFragment
-import java.io.ByteArrayOutputStream
-import java.net.URL
-import java.text.SimpleDateFormat
-import java.util.*
 
 class AiActivity : AppCompatActivity() {
     private val client = OkHttpClient()
-    // creating variables on below line.
     lateinit var txtResponse: TextView
     lateinit var idTVQuestion: TextView
     lateinit var etQuestion: TextInputEditText
@@ -107,7 +58,7 @@ class AiActivity : AppCompatActivity() {
         idTVQuestion.text = question
         etQuestion.setText("")
 
-        val apiKey = "YOUR_API_KEY_HERE"
+        val apiKey = "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"
         val url = "https://api.openai.com/v1/engines/davinci-codex/completions"
 
         val requestBody = """
